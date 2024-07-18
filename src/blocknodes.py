87e2c7bf490da3598ab6950df91a5e0a086758e4
James Marx <james@brewdev.co.za>
2024-07-18 23:05:54 +0100

    handle blank lines

--- src/blocknodes.py
+++ src/blocknodes.py
@@ -1,7 +1,6 @@
-
-
 def markdown_to_blocks(markdown):
     split =  markdown.split("\n\n")
     strip_spaces = map(lambda item: item.strip().replace("  ", "") , split)
-    return list(strip_spaces)
+    filter_empty = filter(lambda item: item != '', strip_spaces)
+    return list(filter_empty)
 
